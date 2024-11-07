d22103006ad55e4ef8d57e9613d7aadb6e7a0b4b
Golam Muntakim <seams123456789@gmail.com>
2024-11-07 13:35:27 -0800

    generic type done

--- module2/2.4.ts
+++ module2/2.4.ts
@@ -10,4 +10,51 @@
             age : 100
         }
     ]
+
+    // interface- generic
+    interface Developer<T>{
+        name : string,
+        computer:{
+            brand : string;
+            model : string;
+            releaseYear : number;
+        },
+        smartWatch: T
+    }
+    const poorDeveloper : Developer<{
+        brand : string,
+        model : string,
+        display : string
+    }> = {
+        name : 'siam',
+        computer : {
+            brand : 'Asus',
+            model : 'X-255UR',
+            releaseYear : 2013,
+        },
+        smartWatch:{
+            brand : 'apple',
+            model : '',
+            display:""
+        }
+    }
+    const richDeveloper : Developer<{
+        brand : string,
+        model: string,
+        display : string,
+        heartTrack : true
+    }> = {
+        name : 'siam',
+        computer : {
+            brand : 'Asus',
+            model : 'X-255UR',
+            releaseYear : 2013,
+        },
+        smartWatch:{
+            brand : 'apple',
+            model : '',
+            display:"",
+            heartTrack : true
+        }
+    }
 }
