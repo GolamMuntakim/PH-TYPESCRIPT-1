561a7a3e6caec1f58e27f4534dbb7e76aafbdc02
Golam Muntakim <seams123456789@gmail.com>
2024-11-08 15:37:31 -0800

    static

---
+++ module3/3.6.ts
@@ -0,0 +1,17 @@
+{
+    // static oop
+    class counter {
+      static  count : number = 0;
+        increment(){
+           return ( counter.count = counter.count+1)
+        }
+        decrement(){
+           return ( counter.count = counter.count-1)
+        }
+    }
+    const instance1 = new counter()
+    console.log(instance1.increment())
+    const instance2 = new counter()
+    console.log(instance2.increment())
+
+}
