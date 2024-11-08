26f8bce0901dd1c3667fce9bd341e3d5cbdc54c8
Golam Muntakim <seams123456789@gmail.com>
2024-11-08 01:08:06 -0800

    oop

---
+++ module3/3.1.ts
@@ -0,0 +1,14 @@
+{
+    // oop - class
+    class animle{
+        name : string;
+        species : string;
+        sound : string;
+        constructor( name : string, species : string, sound:string){
+            this.name = name;
+            this.species = species;
+            this.sound = sound
+        }
+    }
+    const dog = new animle("german", "dog", "ghew")
+}
