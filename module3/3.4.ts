9750a75f419f4898b8b26fb1bb508fa0da2f73e3
Golam Muntakim <seams123456789@gmail.com>
2024-11-08 12:06:28 -0800

    instance of

--- module3/3.4.ts
+++ module3/3.4.ts
@@ -1,4 +1,54 @@
 {
     // type guard using instance of
-    
+    class Animal {
+        name : string;
+        species : string;
+        constructor(name : string, species: string){
+            this.name = name;
+            this.species = species
+        }
+        makeSound(){
+            console.log("I am making sound")
+        }
+    }
+class Dog extends Animal{
+    constructor(name:string , species:string){
+        super ( name, species);
+    }
+    makeBark(){
+        console.log("i am barking")
+    }
+}
+class Cat extends Animal{
+    constructor(name:string , species:string){
+        super ( name, species);
+    }
+    makeMew(){
+        console.log("i am Mewing")
+    }
+}
+// use function to handle smartly 
+const isDog= (animal: Animal):animal is Dog=>{
+    return animal instanceof Dog
+}
+const isCat= (animal: Animal):animal is Cat=>{
+    return animal instanceof Cat
+}
+
+
+
+const getAnimal = (animal : Animal)=>{
+    if(isDog(animal)){
+        animal.makeBark()
+    }else if (isCat(animal)){
+        animal.makeMew()
+    }else {
+        animal.makeSound()
+    }
+}
+const dog  = new Dog("Dog Bhai", "dog")
+const cat = new Cat("Cat Bahi", "cat")
+getAnimal(cat)
+
+    // 
 }
